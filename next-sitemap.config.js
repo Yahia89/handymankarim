9dbb0b5688ee496b10a417d1ad1a3e6384a7bf09
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-03-17 02:06:00 -0700

    try again

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://handymankarim.com', // Replace with your actual custom domain
-    generateRobotsTxt: true, // Generates a robots.txt file
-    outDir: 'out', // This is important for static hosting
-  };
+  siteUrl: 'https://handymankarim.com',
+  generateRobotsTxt: true,
+  outDir: 'out',
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://handymankarim.com/sitemap-0.xml'
+    ]
+  }
+};
   
